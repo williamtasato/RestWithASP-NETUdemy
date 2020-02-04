c5797164feeacb438edd423b7e7c0d6ad030f18f
William_ciandt <william@cisistemas.com.br>
2020-02-04 17:38:16 -0300

    Finish Repository

---
+++ RestWithASPNETUdemy 05 - Spliting Repository/RestWithASPNETUdemy/Business/Implementations/PersonBusinessImpl.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using RestWithASPNETUdemy.Model;
+using RestWithASPNETUdemy.Model.Context;
+using RestWithASPNETUdemy.Repository;
+
+namespace RestWithASPNETUdemy.Business.Implementations
+{
+    public class PersonBusinessImpl : PersonBusiness
+    {
+        private IPersonRepository _repository;
+
+        public PersonBusinessImpl(IPersonRepository repository) {
+            _repository = repository;
+        }
+
+        public Person Create(Person person)
+        {
+                 return _repository.Create (person);
+        }
+
+        public void Delete(long id)
+        {
+            _repository.Delete(id);
+        }
+
+        public List<Person> FindAll()
+        {
+            return _repository.FindAll ();
+        }
+
+        public Person FindById(long id)
+        {
+            return _repository.FindById(id);
+        }
+
+        public Person Update(Person person)
+        {
+            return _repository.Update (person);
+        }
+    }
+}
