d288a5c2fd161658db1fe26e00c784acee4fef27
William_ciandt <william@cisistemas.com.br>
2020-02-04 16:22:03 -0300

    Finish Connection to DataBase

---
+++ RestWithASPNETUdemy 02 - Using Differents Verbs/RestWithASPNETUdemy/Services/Implementations/PersonServiceImpl.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using RestWithASPNETUdemy.Model;
+
+namespace RestWithASPNETUdemy.Services.Implementations
+{
+    public class PersonServiceImpl : IPersonService
+    {
+        private volatile  int count;
+
+        public Person Create(Person person)
+        {
+           return person;
+        }
+
+        public void Delete(long id)
+        {
+           
+        }
+
+        public List<Person> FindAll()
+        {
+            List<Person> persons = new List<Person>();
+
+            for (int i = 0; i < 8; i++) {
+                Person person = MockPerson(i);
+                persons.Add(person);
+            }
+            return persons;
+        }
+
+        private Person MockPerson(int i)
+        {
+            return new Person
+            {
+                Id = IncrementAndGet(),
+                FirstName = "Person Name " +i,
+                LastName = "Person LastName " + i,
+                Adress = "Some Adress " + i,
+                Gender = "Male"
+            };
+        }
+
+        private long IncrementAndGet()
+        {
+            return Interlocked.Increment(ref count);
+        }
+
+        public Person FindById(long id)
+        {
+           return  new Person {
+                Id= IncrementAndGet(),
+                FirstName ="Leandro",
+                LastName ="Costa",
+                Adress ="Uberlandia - Minas Gerais - Brasil",
+                Gender ="Male"
+            };
+        }
+
+        public Person Update(Person person)
+        {
+            return person;
+        }
+    }
+}
--- RestWithASPNETUdemy 03 - Connection to Database/RestWithASPNETUdemy/Startup.cs
+++ RestWithASPNETUdemy 03 - Connection to Database/RestWithASPNETUdemy/Startup.cs
@@ -6,10 +6,12 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using RestWithASPNETUdemy.Model.Context;
 using RestWithASPNETUdemy.Services;
 using RestWithASPNETUdemy.Services.Implementations;
 
@@ -27,6 +29,9 @@ namespace RestWithASPNETUdemy
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            var connection = Configuration["MySqlConnection:MySqlConnectionString"];
+            services.AddDbContext<MySQLContext>(options => options.UseMySql(connection));
+
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
             //Injection Dependency
             services.AddScoped<IPersonService,PersonServiceImpl>();
