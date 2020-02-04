22b10a77a7142967e57d84bbaef271236b38871d
William_ciandt <william@cisistemas.com.br>
2020-02-04 09:21:03 -0300

    implementing all principal verbs

--- RestWithASPNETUdemy 02 - Using Differents Verbs/RestWithASPNETUdemy/Controllers/PersonsController.cs
+++ RestWithASPNETUdemy 02 - Using Differents Verbs/RestWithASPNETUdemy/Controllers/PersonsController.cs
@@ -8,7 +8,7 @@ namespace RestWithASPNETUdemy.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class ValuesController : ControllerBase
+    public class PersonsController : ControllerBase
     {
         // GET api/values
         [HttpGet]
