c9e1d50db1b18b2b3a2fe76b6d3b3530c9530b33
William_ciandt <william@cisistemas.com.br>
2020-02-05 19:09:04 -0300

    aplication VO

---
+++ RestWithASPNETUdemy 08 - Value Object/RestWithASPNETUdemy/Data/Converters/BookConverter.cs
@@ -0,0 +1,50 @@
+﻿using RestWithASPNETUdemy.Data.Converter;
+using RestWithASPNETUdemy.Data.VO;
+using RestWithASPNETUdemy.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace RestWithASPNETUdemy.Data.Converters
+{
+    public class BookConverter : IParser<BookVO, Book>, IParser<Book, BookVO>
+    {
+        public BookVO Parse(Book origin)
+        {
+            if (origin == null) return null;
+            return new BookVO
+            {
+                Id = origin.Id,
+                Author  = origin.Author,
+                LaunchDate = origin.LaunchDate,
+                Price = origin.Price,
+                Title = origin.Title
+            };
+        }
+
+        public Book Parse(BookVO origin)
+        {
+            if (origin == null) return null;
+            return new Book
+            {
+                Id = origin.Id,
+                Author = origin.Author,
+                LaunchDate = origin.LaunchDate,
+                Price = origin.Price,
+                Title = origin.Title
+            };
+        }
+
+        public List<BookVO> ParseList(List<Book> originList)
+        {
+            if (originList == null) return null;
+            return originList.Select(item => Parse(item)).ToList();
+        }
+
+        public List<Book> ParseList(List<BookVO> originList)
+        {
+            if (originList == null) return null;
+            return originList.Select(item => Parse(item)).ToList();
+        }
+    }
+}
