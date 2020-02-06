13df9e74b44bff85dd29eeedfa52e0379700a27e
William_ciandt <william@cisistemas.com.br>
2020-02-06 09:28:47 -0300

    Content Type

---
+++ RestWithASPNETUdemy 10 - Content Negociation/RestWithASPNETUdemy/Data/VO/PersonVO.cs
@@ -0,0 +1,17 @@
+﻿using RestWithASPNETUdemy.Model.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestWithASPNETUdemy.Data.VO
+{
+    public class PersonVO
+    {
+        public long? Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Adress { get; set; }
+        public string Gender { get; set; }
+    }
+}
