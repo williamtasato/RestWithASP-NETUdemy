e1345ce1a3baee3878dc000563444bec4e42e1cc
William_ciandt <william@cisistemas.com.br>
2020-02-07 17:22:12 -0300

    Instalar Windows Service

---
+++ RestWithASPNETUdemy 30 - WindowsService/RestWithASPNETUdemy/Controllers/BooksController.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using RestWithASPNETUdemy.Business;
+using RestWithASPNETUdemy.Data.VO;
+using RestWithASPNETUdemy.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Tapioca.HATEOAS;
+
+namespace RestWithASPNETUdemy.Controllers
+{
+    [ApiVersion("1.0")]
+    [Route("api/[controller]/v{version:ApiVersion}")]
+    [ApiController]
+    public class BooksController: Controller
+    {
+        private IBookBusiness _bookBusiness;
+
+        public BooksController(IBookBusiness bookBusiness)
+        {
+            _bookBusiness = bookBusiness;
+
+        }
+
+        // GET api/values
+        [HttpGet]
+        [Authorize("Bearer")]
+        public IActionResult Get()
+        {
+            return Ok(_bookBusiness.FindAll());
+            }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        [Authorize("Bearer")]
+        [TypeFilter(typeof(HyperMediaFilter))]
+        public IActionResult Get(int id)
+        {
+            var book = _bookBusiness.FindById(id);
+            if (book == null) return NotFound();
+            return Ok(book);
+
+        }
+
+        // POST api/values
+        [HttpPost]
+        [Authorize("Bearer")]
+        [TypeFilter(typeof(HyperMediaFilter))]
+        public IActionResult Post([FromBody] BookVO book)
+        {
+            if (book == null) return BadRequest();
+            return new ObjectResult(_bookBusiness.Create(book));
+               }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        [Authorize("Bearer")]
+        [TypeFilter(typeof(HyperMediaFilter))]
+        public IActionResult Put([FromBody] BookVO book)
+        {
+            if (book == null) return BadRequest();
+            var updateBook = _bookBusiness.Update(book);
+            if (updateBook == null) return NoContent();
+            return new ObjectResult(updateBook);
+
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        [Authorize("Bearer")]
+        [TypeFilter(typeof(HyperMediaFilter))]
+        public IActionResult Delete(int id)
+        {
+            _bookBusiness.Delete(id);
+            return NoContent();
+        }
+    }
+}
+
---
+++ RestWithASPNETUdemy 30 - WindowsService/RestWithASPNETUdemy/Model/User.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestWithASPNETUdemy.Model
+{
+    public class User
+    {
+        public long? id { get; set; }
+        public string Login { get; set; }
+        public string AcessKey { get; set; }
+    }
+}
